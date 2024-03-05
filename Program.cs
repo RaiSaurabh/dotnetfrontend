5e41cf25a8dd4d742ed304ba34a6252b8b0bd7b9
Saurabh Rai <rai.saurabh@hotmail.com>
2024-03-05 17:28:56 +0530

    Upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using ASP_NET_Core_App.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("StaffInfo"));
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
